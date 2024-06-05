671319ba00c306b77ef06cdc19a6ecaf92997ea5
Mariobpb <mariobpb27@gmail.com>
2024-06-05 12:15:37 -0600

    Implementar API

---
+++ PilluleBoxAPI/app.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+app.use(express.json());
+
+const connection = mysql.createConnection({
+    host: '35.239.105.175',
+    user: 'root',
+    password: 'proyectomalo20100335$',
+    database: 'dbtest'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos: ', err);
+    return;
+  }
+  console.log('Conectado a la base de datos MySQL');
+});
+
+app.get('/registros', (req, res) => {
+  const query = 'SELECT * FROM Registros';
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error al obtener los datos' });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.post('/registros', (req, res) => {
+  const { led, fecha, hora } = req.body;
+  const query = 'INSERT INTO Registros (led, fecha, hora) VALUES (?, ?, ?)';
+  connection.query(query, [led, fecha, hora], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error al agregar un nuevo dato' });
+      return;
+    }
+    res.json({ id: result.insertId });
+  });
+});
+
+app.delete('/registros/:id', (req, res) => {
+  const id = req.params.id;
+  const query = 'DELETE FROM Registros WHERE id = ?';
+  connection.query(query, [id], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error al eliminar el registro' });
+      return;
+    }
+    if (result.affectedRows === 0) {
+      res.status(404).json({ error: 'Registro no encontrado' });
+      return;
+    }
+    res.json({ message: 'Registro eliminado correctamente' });
+  });
+});
+
+app.patch('/registros/:id', (req, res) => {
+  const id = req.params.id;
+  const { led, fecha, hora } = req.body;
+  const query = 'UPDATE Registros SET led = ?, fecha = ?, hora = ? WHERE id = ?';
+  connection.query(query, [led, fecha, hora, id], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error al actualizar el registro' });
+      return;
+    }
+    if (result.affectedRows === 0) {
+      res.status(404).json({ error: 'Registro no encontrado' });
+      return;
+    }
+    res.json({ message: 'Registro actualizado correctamente' });
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
